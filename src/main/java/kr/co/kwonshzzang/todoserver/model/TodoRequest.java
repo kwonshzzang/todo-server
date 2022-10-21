00425978cd48dcf1e247d15c05934a51dae8d37e
kwonshzzang <105833021+kwonshzzang@users.noreply.github.com>
2022-10-21 17:32:07 +0900

    2022.10.21

---
+++ src/main/java/kr/co/kwonshzzang/todoserver/model/TodoRequest.java
@@ -0,0 +1,14 @@
+package kr.co.kwonshzzang.todoserver.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoRequest {
+    private String title;
+    private Long order;
+    private boolean completed;
+}
---
+++ src/main/java/kr/co/kwonshzzang/todoserver/model/TodoResponse.java
@@ -0,0 +1,24 @@
+package kr.co.kwonshzzang.todoserver.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoResponse {
+    private Long id;
+    private String title;
+    private Long order;
+    private boolean completed;
+    private String url;
+
+    public TodoResponse(TodoEntity todoEntity) {
+        this.id = todoEntity.getId();
+        this.title = todoEntity.getTitle();
+        this.order = todoEntity.getOrder();
+        this.completed = todoEntity.isComplete();
+        this.url = "http://localhost:8080/" + this.id;
+    }
+}
